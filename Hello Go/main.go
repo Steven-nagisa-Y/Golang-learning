e18304fb2f8abe0b3aa5c0c3200dfb380e1a7cf2
AiRocket <steven_nagisa@qq.com>
2019-12-19 10:43:18 +0800

    Yes

--- Hello Go/main.go
+++ Hello Go/main.go
@@ -1,24 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main() {
-	s := []int{2, 3, 5, 7, 11, 13}
-	printSlice(s)
-
-	// 截取切片使其长度为 0
-	s = s[:0]
-	printSlice(s)
-
-	// 拓展其长度
-	s = s[:4]
-	printSlice(s)
-
-	// 舍弃前两个值
-	s = s[2:]
-	printSlice(s)
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
 }
 
-func printSlice(s []int) {
-	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+func main() {
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	fmt.Println(hypot(5, 12))
+	fmt.Println(compute(hypot))
+	fmt.Println(compute(math.Pow))
 }
